f5c8167083eeced3aba94ee54f118424572c6c99
TilakSai <tilakkoniki.sai@gmail.com>
2025-09-01 11:18:14 -0700

    Interesting code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int num1 = 10; jidhgudfgi
+        int num1 = 10;
         int num2 = 20;
         int sum = num1 + num2;
         System.out.println("First Number: " + num1);
